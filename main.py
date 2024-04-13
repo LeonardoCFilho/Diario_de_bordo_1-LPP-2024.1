4541cf07a546bf2d36eb31a38a83ea4244d023d0
LeonardoCFilho <131294934+LeonardoCFilho@users.noreply.github.com>
2024-04-13 18:47:05 -0300

    2.Exemplo_tipos_variaveis.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#Demonstracao dos tipos de variaveis
+
+Int = 2 #Inteiros
+print(type(Int))
+
+Float = 3.14 #Decimais
+print(type(Float))
+
+String = "Hello World" #Caracter(es)
+print(type(String))
+
+Bool = True #Booleano
+print(type(Bool))
+
+Complex = 5+2j #Numeros complexos (Real + Imaginario)
+print(type(Complex))
+
+List = [1, 2.1, 3.2, 4.3, 5.4] #Lista/Vetor
+print(type(List))
+
+Tuple = (1, 2, 3) #Lista/Vetor Imutavel
+print(type(Tuple))
+
+Dict = {'Caso 1': 1, 'Caso 2': 2} #Lista/Vetor com elementos agrupados por chave e valor
+print(type(Dict))
